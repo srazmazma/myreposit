2ed02d23d3667a760d770a82f6f2854ac6808c09
srazmazma <34821337+srazmazma@users.noreply.github.com>
2019-06-03 23:56:15 -0700

    Add files via upload

---
+++ OpenBrowser1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import webbrowser
+url_base = "https://www.bluemonday.io"
+url_extension = "/blog"
+url = url_base + url_extension
+# webbrowser.open_new_tab (url_base)
+webbrowser.open_new_tab (url)
+########
+
+
+# In[ ]:
+
+
+
+
